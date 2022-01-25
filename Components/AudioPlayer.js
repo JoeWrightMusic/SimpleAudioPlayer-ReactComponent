4c196bcabddf196995df5da6e1fa18200b705b73
Joe Wright <35366767+JoeWrightMusic@users.noreply.github.com>
2022-01-25 18:41:12 +0000

    Working Draft Done

---
+++ Components/AudioPlayer.js
@@ -0,0 +1,137 @@
+import React, {useState, useRef, useEffect} from "react"
+import styles from "../Styles/AudioPlayer.module.css"
+import {MdKeyboardArrowLeft, MdKeyboardArrowRight} from "react-icons/md"
+import {IoPlayOutline} from "react-icons/io5"
+import {IoPauseOutline} from "react-icons/io5"
+
+
+function AudioPlayer(){
+    // state
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+
+    // references
+    const audioPlayer = useRef(); //reference audio component
+    const progressBar = useRef(); //reference progress bar
+    const animationRef = useRef(); //reference animation
+
+    // effect
+    useEffect(() => {
+        const seconds = Math.floor(audioPlayer.current.duration)
+        setDuration(seconds)
+        progressBar.current.max = seconds;
+    }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+    const onLoadedMetadata = () => {
+        const seconds = Math.floor(audioPlayer.current.duration)
+        setDuration(seconds)
+        progressBar.current.max = seconds;
+    }
+
+    //calc time
+    const calculateTime = (secs) => {
+        const minutes = Math.floor(secs/60)
+        const returnMinutes = minutes<10 ? `0${minutes}` : `${minutes}`
+        const seconds = Math.floor(secs%60)
+        const returnSeconds = seconds<10 ? `0${seconds}` : `${seconds}`
+        return `${returnMinutes}:${returnSeconds}`
+    }
+
+    //toggle playback
+    const togglePlayPause = () => {
+        const prevPlayValue = isPlaying;
+        setIsPlaying(!prevPlayValue)
+        if(!prevPlayValue){
+            audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying)
+        } else {
+            audioPlayer.current.pause();
+            cancelAnimationFrame(animationRef.current)
+        }
+    }
+
+    const whilePlaying = () => {
+        progressBar.current.value = audioPlayer.current.currentTime
+        changePlayerCurrentTime()
+        if (audioPlayer.current.ended){
+            setIsPlaying(false)
+        }
+        animationRef.current = requestAnimationFrame(whilePlaying)
+
+    }
+
+    //change range
+    const changeRange = () => {
+        audioPlayer.current.currentTime = progressBar.current.value
+        changePlayerCurrentTime();
+    }
+
+    const changePlayerCurrentTime = () => {
+        progressBar.current.style.setProperty('--seekBeforeWidth', `${progressBar.current.value / duration * 100}%`)
+        setCurrentTime(progressBar.current.value)
+    }
+
+
+    //30s nav buttons
+    const backThirty = () => {
+        progressBar.current.value = Number(progressBar.current.value - 30)
+        changeRange();
+    }
+    const forwardThirty = () => {
+        progressBar.current.value = Number(progressBar.current.value + 30)
+        changeRange();
+    }
+
+    console.log((duration && !isNaN(duration)))
+    return(
+        <div className={styles.audioPlayer}>
+            <audio 
+                src=">>LINK TO SF HERE<<" 
+                preload="metadata"
+                ref={audioPlayer}
+                onLoadedMetadata={onLoadedMetadata}
+            />
+            {/* TRANSPORT */}
+            <button 
+                className={styles.forwardBackward}
+                onClick={backThirty}
+            >
+                <MdKeyboardArrowLeft/> 30
+            </button>
+            <button
+                onClick={togglePlayPause}
+                className={styles.playPause}
+            >
+                {isPlaying ? <IoPauseOutline/> : <IoPlayOutline className={styles.play}/>}
+            </button>
+            <button 
+                className={styles.forwardBackward}
+                onClick={forwardThirty}
+            >
+                30 <MdKeyboardArrowRight/>
+            </button>
+
+            {/* CURRENT TIME */}
+            <div className={styles.currentTime}>{calculateTime(currentTime)}</div>
+
+            {/* PROGRESS BAR */}
+            <div>
+                <input 
+                    className={styles.progressBar} 
+                    type="range"
+                    defaultValue="0"
+                    ref={progressBar}
+                    onChange={changeRange}
+                />
+            </div>
+            {/* DURATION */}
+            <div className={styles.duration}>
+                {!isNaN(duration) ? calculateTime(duration) : "loading..."}
+            </div>
+
+        </div>
+    )
+}
+
+export default AudioPlayer 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SimpleAudioPlayer-ReactComponent
- Simple Audio Player Component following Amy Dutton(Self Teach Me)'s Tutorial 
+
+ Simple Audio Player Component following Amy Dutton's (Self Teach Me) Tutorial:
+[How to Build a Custom Audio Player in React](https://www.youtube.com/watch?v=sqpg1qzJCGQ)
+
+In addition to the video content, an additional function is added to the audio components 'onLoadedMetadata' event listener (as suggested in the pinned comment for the video), and the playbutton resets when the file plays to the end (audioPlayer.current.ended===true).
